b3bb088195fb5c77e0f5e8165d3a4b5347d2d301
joey <pball626@gmail.com>
2021-02-28 19:10:02 -0600

    commit tree 5eed2e775fe7aa8231dc36c44a358da72079ba18

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require "pry"
+class School
+
+    attr_reader :name, :roster
+    def initialize(roster)
+        @roster = {}
+        @name = name
+    end
+
+    def add_student(name, grade)
+        @roster[grade] = []
+        @roster[grade] << name
+    end
+
+  
+end
+
+
+# hash["new_key"] = []
+# hash["new_key"] << "new_value_for_value_array"
+
+# hash
+#  => {"new_key"=>["new_value_for_value_array"]}  
